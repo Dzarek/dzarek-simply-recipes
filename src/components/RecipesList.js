83632f9a577897780c70fc3f8919679c625a75fd
Jarek <jarekjanas95@gmail.com>
2021-08-19 23:35:13 +0200

    commit

--- src/components/RecipesList.js
+++ src/components/RecipesList.js
@@ -7,7 +7,7 @@ const RecipesList = ({ recipes = [] }) => {
   return (
     <div className="recipes-list">
       {recipes.map(recipe => {
-        const { id, title, image, prepTime, cookTime } = recipe
+        const { id, title, image, prepTIme, cookTime } = recipe
         const pathToImage = getImage(image)
         const slug = slugify(title, { lower: true })
         return (
@@ -19,7 +19,7 @@ const RecipesList = ({ recipes = [] }) => {
             />
             <h5>{title}</h5>
             <p>
-              Prep: {prepTime}min | Cook: {cookTime}min
+              Prep: {prepTIme}min | Cook: {cookTime}min
             </p>
           </Link>
         )
