c6cf60aa815dab11d16f3299f52a38d9ce77ce5c
Jarek <jarekjanas95@gmail.com>
2021-08-19 23:03:17 +0200

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,32 @@
 import React from "react"
+import { Link } from "gatsby"
+import Layout from "../components/Layout"
+import { StaticImage } from "gatsby-plugin-image"
+import AllRecipes from "../components/AllRecipes"
+import SEO from "../components/SEO"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <main className="page">
+        <header className="hero">
+          <StaticImage
+            src="../assets/images/main.jpeg"
+            alt="main"
+            className="hero-img"
+            placeholder="tracedSVG"
+            layout="fullWidth"
+          ></StaticImage>
+          <div className="hero-container">
+            <div className="hero-text">
+              <h1>simply recipes</h1>
+              <h4>no fluff, just recipes</h4>
+            </div>
+          </div>
+        </header>
+        <AllRecipes />
+      </main>
+    </Layout>
+  )
 }
